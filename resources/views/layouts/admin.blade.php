1694e81ea7dc54320fbd491b77d0c130bcb55b53
gianmarcodiale <98177039+gianmarcodiale@users.noreply.github.com>
2022-06-21 17:44:30 +0200

    corrections

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -24,7 +24,7 @@
 <body>
     <div id="app">
         <nav class="navbar navbar-dark sticky-top bg-secondary flex-md-nowrap p-0 shadow">
-            <a class="navbar-brand col-md-3 col-lg-2 mr-0 px-3 text-uppercase" href="#">boolpress</a>
+            <a class="navbar-brand col-md-3 col-lg-2 mr-0 px-3 text-uppercase font-weight-bold" href="#">boolpress</a>
             <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-toggle="collapse"
                 data-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false"
                 aria-label="Toggle navigation">
